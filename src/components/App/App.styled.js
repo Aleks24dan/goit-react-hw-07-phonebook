2966dd5124b0988b52bfb2157030925695695797
Aleks24dan <aleks24dan@gmail.com>
2021-10-19 22:18:56 +0200

    gdf
    fsdfg

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin: 20px;
+
+  background: linear-gradient(45deg, rgb(160, 239, 240), rgb(8, 48, 140));
+  padding: 10px;
+  width: 700px;
+  box-shadow: 5px 6px 9px rgb(11, 93, 194);
+  border-radius: 8px;
+  text-align: center;
+`;
+export const TitleH1 = styled.h1`
+  font-style: italic;
+`;
+
+export const TitleH2 = styled.h2`
+  font-style: italic;
+  margin-top: 46px;
+`;
